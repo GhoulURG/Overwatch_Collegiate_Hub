e2e0502e978cd77f9d1ef727c85a46d161251649
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2025-11-16 17:28:37 -0500

    Update auth.js

--- UserManagement/middleware/auth.js
+++ UserManagement/middleware/auth.js
@@ -1,1 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+  const token = req.header('x-auth-token');
+  if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: 'Token is not valid' });
+  }
+}
+
+module.exports = auth;
 
