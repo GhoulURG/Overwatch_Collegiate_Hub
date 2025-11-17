0b002ea1b6ddd8e9311417f31e85b2d95c7395a5
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2025-11-16 21:58:01 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+  <nav>
+    <Link to="/signup">Signup</Link> | 
+    <Link to="/login">Login</Link> | 
+    <Link to="/profile">Profile</Link>
+  </nav>
+);
+
+export default Navbar;
 
